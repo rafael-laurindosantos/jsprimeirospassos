ec642b396f7b4ec67b1950cf69486cfb25468af4
Rafael Laurindo dos Santos <rafael.laurindosantos@gmail.com>
2026-06-01 22:19:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// // // // //variável que varia
+// // // // //let
+
+// // // // //constante não varia
+// // // // //const
+
+// // // // // = atribuição
+
+// // // // let ze;
+// // // // const manga = 40;
+
+// // // // ze = 10-100;
+
+// // // // ze = 2;
+// // // // alert(ze);
+
+// // // let nome;
+// // // let sobrenome;
+// // // const idade = 35;
+
+// // // nome = 'Rafael ';
+// // // sobrenome = 'Santos ';
+
+// // // alert(nome + sobrenome + idade);
+
+// // let roberto = 10;
+// // let carlos = 12;
+
+// // alert(roberto + carlos);
+
+// // roberto = -3;
+// // carlos = 5;
+
+// // alert(roberto + carlos);
+
+// let primeiroNumero = +prompt('Digite o primeiro número');
+// let segundoNumero = +prompt('Digite o segundo número');
+
+// if(segundoNumero != 0) {
+//     alert(primeiroNumero / segundoNumero);
+// }
+
+// alert(primeiroNumero + segundoNumero);
+
+
+// alert(primeiroNumero - segundoNumero);
+
+
+// alert(primeiroNumero * segundoNumero);
+
+const nome = prompt('Digite um nome');
+
+
+if(nome == 'Rafael') {
+
+    alert('Digite outro nome');
+}
